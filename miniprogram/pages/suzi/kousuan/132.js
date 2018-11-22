f9b7c188ea51b5f46364347e906e0cb0dd480e9a
wangfan <fan.wang@colorcun.com>
2018-11-22 18:17:31 +0800

    shuzi

---
+++ miniprogram/pages/suzi/kousuan/132.js
@@ -0,0 +1,75 @@
+// miniprogram/pages/suzi/kousuan/132.js
+const {regeneratorRuntime} = getApp()
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        width:100,
+        height:100,
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+      wx.setKeepScreenOn({
+        keepScreenOn: true
+      })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: async function () {
+        let sys=wx.getSystemInfoSync();
+        console.log(sys)
+      this.setData({
+        width:sys.windowHeight,
+        height:sys.windowWidth,
+      })
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
