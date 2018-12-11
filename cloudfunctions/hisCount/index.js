3f3fe31b8e8c1c511122d3ca2cc337251fa6c086
wangfan <fan.wang@colorcun.com>
2018-12-11 14:18:39 +0800

    shuzi

---
+++ cloudfunctions/hisCount/index.js
@@ -0,0 +1,41 @@
+// 云函数模板
+// 部署：在 cloud-functions/login 文件夹右击选择 “上传并部署”
+
+/**
+ * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端
+ *
+ * event 参数包含
+ * - 小程序端调用传入的 data
+ * - 经过微信鉴权直接可信的用户唯一标识 openid
+ *
+ */
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db = cloud.database()
+exports.main = async (event, context) => {
+  var openid=event.userInfo.openId;
+  var total=0,last_addtime=0,addtime=0;
+  try{
+    var his=await db.collection('shuzi132').field({
+      addtime:true,
+    }).where({
+      _openid:openid
+    }).orderBy('addtime', 'desc').limit(200).get();
+    if(his.data.length>0){
+      his.data.forEach(r=>{
+        if(last_addtime==0){
+          last_addtime=r.adddtime;
+        }
+        addtime=r.adddtime;
+        if(addtime<r.adddtime-43200){
+          total=+1;
+        }
+      })
+    }
+  }catch (e){
+
+  }
+
+  if(his.data)
+  return {total:total,last_addtime:last_addtime}
+}
