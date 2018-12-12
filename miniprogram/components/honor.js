df40b5538cf351f32786d7f59b82f3ffa77e28be
wangfan <fan.wang@colorcun.com>
2018-12-12 12:26:25 +0800

    shuzi

---
+++ miniprogram/components/honor.js
@@ -0,0 +1,33 @@
+/**
+ * Created by goto9 on 2018/12/12.
+ */
+const honor=[
+  {icon:'sx',count:12,ext:'png',hasTop:true,hasBottom:true,title:'生肖宠兽',thumb:'sx.png'},
+  {icon:'nsj',count:11,ext:'png',title:'南山宠兽',thumb:'nsj%20%281%29.png'},
+  {icon:'ncj',count:13,ext:'jpg',title:'南次宠兽',thumb:'ncj%20%281%29.jpg'},
+  {icon:'xsj',count:18,ext:'png',title:'西山宠兽',thumb:'xsj%20%281%29.png'},
+  {icon:'long',count:9,ext:'jpg',hasTop:true,title:'龙子九兽',thumb:'long.jpg',
+    info:'龙生九子是指龙生九个儿子，九个儿子都不成龙，各有不同；九子分别是：囚牛、睚眦、嘲风、蒲牢、狻猊、赑屃、狴犴、负屃、螭吻'}
+]
+export default {
+  honor:honor,
+  formatNum($num){
+    let data={}
+    honor.forEach(h=>{
+      data[h.icon]=0;
+    });
+    let icon_index=0;
+    for(let i=1;i<=$num;i++){
+      if(i%6==0){
+        data['long']+=1;
+      }else{
+        let ic=honor[icon_index];
+        data[ic.icon]+=1;
+        if(data[ic.icon]+1>=ic.count){
+          icon_index+=1;
+        }
+      }
+    }
+    return data;
+  }
+}
---
+++ miniprogram/pages/me/honor.js
@@ -0,0 +1,61 @@
+// miniprogram/pages/me/honor.js
+const app = getApp()
+const {regeneratorRuntime} = app
+import ho from '../../components/honor';
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      honor:ho.honor,
+      honorNum:{},
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+      app.cloudHisCount().then(res=>{
+          let honorNum=ho.formatNum(res.total);
+          this.setData({
+            honorNum:honorNum,
+          })
+      })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
